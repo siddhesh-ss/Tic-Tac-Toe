104aa631fb3d95a63550461f0e05ab4b5dffe168
siddhesh-ss <siddheshshelar32@gmail.com>
2026-08-29 21:58:13 +0000

    Added ScreenController Connected console based game to interactive page.

--- script.js
+++ script.js
@@ -14,13 +14,17 @@ const GameBoard = (() => {
             token : 'X', 
         }
     ];
-    
+
     for(let i = 0 ; i < row ; i++) {
         board[i] = [];
         for(let j = 0 ; j < row ; j++) {
             board[i].push(Cell());
         }
     }
+
+    const resetBoard = () => {
+        board.map(row => row.map(cell => cell.addToken('-')));
+    };
 
     const getBoard  = () => board;
 
@@ -35,8 +39,16 @@ const GameBoard = (() => {
         }
 
     const dropToken = (row, col, player) => {
-        if(board[row][col].getValue() === '-') board[row][col].addToken(player.token);
-        else return;
+        board[row][col].addToken(player.token);
+    }
+
+    const isBoardFull = () => {
+        for(let row of board) {
+            for(let col of row) {
+                if(col.getValue() === '-') return false;
+            }
+        }
+        return true;
     }
 
     return {
@@ -45,6 +57,9 @@ const GameBoard = (() => {
         getPlayer,
         printBoard,
         dropToken,
+        resetBoard,
+        isBoardFull,
+        row,
     }
 })();
 
@@ -62,20 +77,30 @@ function Cell() {
 }
 
 function GameController() {
-    const printNewRound = () => {
-        GameBoard.printBoard();
-        console.log(`${GameBoard.getPlayer().playerName}'s turn...`);
+    const board = GameBoard.getBoard();
+
+    function isWin(row, col) {
+        if(board[row][0].getValue() === board[row][1].getValue() && board[row][1].getValue() === board[row][2].getValue()) return true;
+        if(board[0][col].getValue() === board[1][col].getValue() && board[1][col].getValue() === board[2][col].getValue()) return true;
+
+        if(row === col && board[0][0].getValue() === board[1][1].getValue() && board[1][1].getValue() === board[2][2].getValue()) return true;
+        if(row + col === 2 && board[0][2].getValue() === board[1][1].getValue() && board[1][1].getValue() === board[2][0].getValue()) return true;
+
+        return false;
     };
 
     const playRound = (row, col) => {
+        if(board[row][col].getValue() !== '-') return;
         console.log(`${GameBoard.getPlayer().token} is dropped by ${GameBoard.getPlayer().playerName}`);
         GameBoard.dropToken(row, col, GameBoard.getPlayer());
 
+        // Winning Condition.
+        if(isWin(row, col)) return "WIN";
+        else if(GameBoard.isBoardFull()) return "TIE";
+
         GameBoard.switchPlayer();
-        printNewRound();
     }
 
-    printNewRound();
     return {
         playRound,
     };
@@ -83,4 +108,35 @@ function GameController() {
 
 const game = GameController();
 
+function displayBoard() {
+    const board = GameBoard.getBoard();
+    const boardBox = document.getElementById("board");
+    boardBox.textContent = "";
+    for(let r = 0 ; r < 3 ; r++) {
+        const row = document.createElement("div");
+        for(let c = 0 ; c < 3 ; c++) {
+            const col = document.createElement("button");
+            col.textContent = String(board[r][c].getValue());
+            col.classList.add("cell");
+            col.dataset.row = r;
+            col.dataset.col = c;
+            row.appendChild(col);
+        }
+        row.classList.add("row");
+        boardBox.appendChild(row);
+    }
+}
 
+displayBoard();
+function screenController() {
+    const boardBox = document.getElementById("board");
+
+    boardBox.addEventListener("click", (e) => {
+        const selectedRow = e.target.dataset.row;
+        const selectedCol = e.target.dataset.col;
+        if(!selectedCol || !selectedRow) return;
+        game.playRound(selectedRow, selectedCol);
+        displayBoard();
+    })
+}
+screenController();
